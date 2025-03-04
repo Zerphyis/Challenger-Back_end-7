0e367352927b12a207f0269ff46d6e631ef7a766
Otavio <estudosprogotavio@gmail.com>
2025-03-04 10:21:41 -0300

    Commit inicial

---
+++ src/main/java/dev/Zerphyis/Trips/TripsApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.Trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsApplication.class, args);
+	}
+
+}
