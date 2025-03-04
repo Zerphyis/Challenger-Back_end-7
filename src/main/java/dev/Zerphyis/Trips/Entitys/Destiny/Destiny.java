e674db582b65cac79394b9a5d1c9720d0cd6da6a
Otavio <estudosprogotavio@gmail.com>
2025-03-04 12:46:35 -0300

    Adicionando novos atribuidos a entidade

--- src/main/java/dev/Zerphyis/Trips/Entitys/Destiny/Destiny.java
+++ src/main/java/dev/Zerphyis/Trips/Entitys/Destiny/Destiny.java
@@ -3,6 +3,7 @@ package dev.Zerphyis.Trips.Entitys.Destiny;
 import dev.Zerphyis.Trips.Entitys.Dtos.DadosDestiny;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Tb_Destino")
@@ -15,14 +16,22 @@ public class Destiny {
     @NotBlank
     private  String photo;
     @NotBlank
-    private  String price;
+    private String photo2;
+
+    @NotBlank
+    @Size(max = 160, message = "Meta deve ter no máximo 160 caracteres")
+    private String meta;
+
+    private String description;
     public  Destiny(){
 
     }
-    public  Destiny(DadosDestiny data){
-        this.name= data.name();
-        this.photo= data.photo();
-        this.price= data.price();
+    public Destiny(DadosDestiny data) {
+        this.name = data.name();
+        this.photo = data.photo();
+        this.photo2 = data.photo2();
+        this.meta = data.meta();
+        this.description = data.description();
     }
 
     public String getName() {
@@ -41,11 +50,27 @@ public class Destiny {
         this.photo = photo;
     }
 
-    public String getPrice() {
-        return price;
+    public String getPhoto2() {
+        return photo2;
     }
 
-    public void setPrice(String price) {
-        this.price = price;
+    public void setPhoto2(String photo2) {
+        this.photo2 = photo2;
+    }
+
+    public String getMeta() {
+        return meta;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
