dd795e48ab2e70c228e6363188235826d526f4f7
Otavio <estudosprogotavio@gmail.com>
2025-03-04 10:22:34 -0300

    Dto da Entidade Depoimento

---
+++ src/main/java/dev/Zerphyis/Trips/Entitys/Dtos/DadosTestimonial.java
@@ -0,0 +1,8 @@
+package dev.Zerphyis.Trips.Entitys.Dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosTestimonial( String photo,
+          String testimonial,
+         String namePersonTestimonial ) {
+}
